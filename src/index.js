f3087ab0b3aad8475e827cf089172e92b90bbf59
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-29 20:11:47 -0300

    Sending interactions up component trees

--- src/index.js
+++ src/index.js
@@ -9,14 +9,31 @@ const createArray = (length) => [
   ...Array(length)
 ];
 
-function Star({ selected = false }) {
-  return <FaStar color={ selected ? "yellow" : "gray" } />;
+function Star({ selected = false, onSelect }) {
+  return (
+    <FaStar 
+      color={ selected ? "yellow" : "gray" }
+      onClick={onSelect} 
+    />
+  );
 }
 
 function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => (
-    <Star key={i} />
-  ));
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star 
+          key={i} 
+          selected={selectedStars > i}
+          onSelect={() => { setSelectedStars(i + 1) }} 
+        />
+      ))}
+
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
 }
 
 function App() {
