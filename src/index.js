a781fa40410a1dc11375db1d94b6fe5fe300aab6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-29 20:16:37 -0300

    Introducing useEffect

--- src/index.js
+++ src/index.js
@@ -1,43 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FaStar } from 'react-icons/fa';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const createArray = (length) => [
-  ...Array(length)
-];
-
-function Star({ selected = false, onSelect }) {
-  return (
-    <FaStar 
-      color={ selected ? "yellow" : "gray" }
-      onClick={onSelect} 
-    />
-  );
-}
-
-function StarRating({ totalStars = 5 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star 
-          key={i} 
-          selected={selectedStars > i}
-          onSelect={() => { setSelectedStars(i + 1) }} 
-        />
-      ))}
-
-      <p>{selectedStars} of {totalStars}</p>
-    </>
-  );
-}
-
 function App() {
-  return <StarRating totalStars={10} />;
+  const [name, setName] = useState("Lucas");
+
+  useEffect(() => {
+    document.title = `Celebrate ${name}`;
+  });
+
+  return (
+    <section>
+      <p>Congratulations {name}!</p>
+
+      <button onClick={() => setName("Luke")}>Change Winner</button>
+    </section>
+  );
 }
 
 root.render(
