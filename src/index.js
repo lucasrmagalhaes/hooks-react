d84df8aeee57bf58948b1cc70be53ce503823e64
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-29 20:22:26 -0300

    Working with the dependency array

--- src/index.js
+++ src/index.js
@@ -6,16 +6,29 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App() {
   const [name, setName] = useState("Lucas");
+  const [admin, setAdmin] = useState(false);
 
   useEffect(() => {
-    document.title = `Celebrate ${name}`;
-  });
+    console.log(`Celebrate ${name}`);
+  }, [name]);
+
+  useEffect(() => {
+    console.log(`The user is: ${ admin ? "admin" : "not admin" }`);
+  }, [admin]);
 
   return (
     <section>
       <p>Congratulations {name}!</p>
 
       <button onClick={() => setName("Luke")}>Change Winner</button>
+
+      <p>{ admin ? "logged in" : "not logged in" }</p>
+
+      <button
+        onClick={() => setAdmin(true)}
+      >
+        Log In
+      </button>
     </section>
   );
 }
